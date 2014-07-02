8b2f7c8b795881862e785c2825b15b9d90606faa
Robert F. Nash <robert@robertnash.net>
2014-07-02 02:54:45 -0400

    vagrant: my first modifying attempt

--- pkgs/development/tools/vagrant/default.nix
+++ pkgs/development/tools/vagrant/default.nix
@@ -3,19 +3,24 @@
 
 assert stdenv.system == "x86_64-linux" || stdenv.system == "i686-linux";
 
+let
+  version = "1.4.3";
+in
 stdenv.mkDerivation rec {
-  name = "vagrant-1.4.3";
+  name = "vagrant-${version}";
 
   src =
     if stdenv.system == "x86_64-linux" then
       fetchurl {
-        url    = https://dl.bintray.com/mitchellh/vagrant/vagrant_1.4.3_x86_64.deb;
+        url    = "https://dl.bintray.com/mitchellh/vagrant/vagrant_${version}_x86_64.deb";
         sha256 = "dbd06de0f3560e2d046448d627bca0cbb0ee34b036ef605aa87ed20e6ad2684b";
+#        sha256 = "1gmdg92dw7afnvpji0wg4nzr7vhk8mrmcqk3hcrkwscby2f2bhqg";
       }
     else
       fetchurl {
-        url    = https://dl.bintray.com/mitchellh/vagrant/vagrant_1.4.3_i686.deb;
+        url    = "https://dl.bintray.com/mitchellh/vagrant/vagrant_${version}_i686.deb";
         sha256 = "66e613fc1c9e31ecaf8e5f1d07d2ae4fca3d4fc2e43593543962664258d9af9b";
+#        sha256 = "1z26b6yghqgx8jbi2igf4kk4h6rzy869gli2vj7ayl7vbqdfvb60";
       };
 
   meta = with stdenv.lib; {
@@ -34,48 +39,6 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     sed -i "s|/opt|$out/opt|" usr/bin/vagrant
-
-    # overwrite embedded binaries
-
-    # curl: curl
-    rm opt/vagrant/embedded/bin/curl
-    ln -s ${curl}/bin/curl opt/vagrant/embedded/bin
-
-    # libarchive: bsdtar, bsdcpio
-    rm opt/vagrant/embedded/bin/{bsdtar,bsdcpio}
-    ln -s ${libarchive}/bin/bsdtar opt/vagrant/embedded/bin
-    ln -s ${libarchive}/bin/bsdcpio opt/vagrant/embedded/bin
-
-    # openssl: c_rehash, openssl
-    rm opt/vagrant/embedded/bin/{c_rehash,openssl}
-    ln -s ${openssl}/bin/c_rehash opt/vagrant/embedded/bin
-    ln -s ${openssl}/bin/openssl opt/vagrant/embedded/bin
-
-    # ruby: erb, gem, irb, rake, rdoc, ri, ruby, testrb
-    rm opt/vagrant/embedded/bin/{erb,gem,irb,rake,rdoc,ri,ruby,testrb}
-    ln -s ${ruby}/bin/erb opt/vagrant/embedded/bin
-    ln -s ${ruby}/bin/gem opt/vagrant/embedded/bin
-    ln -s ${ruby}/bin/irb opt/vagrant/embedded/bin
-    ln -s ${rubyLibs.rake}/bin/rake opt/vagrant/embedded/bin
-    ln -s ${ruby}/bin/rdoc opt/vagrant/embedded/bin
-    ln -s ${ruby}/bin/ri opt/vagrant/embedded/bin
-    ln -s ${ruby}/bin/ruby opt/vagrant/embedded/bin
-    ln -s ${ruby}/bin/testrb opt/vagrant/embedded/bin
-
-    # libiconv: iconv
-    rm opt/vagrant/embedded/bin/iconv
-    ln -s ${libiconv}/bin/iconv opt/vagrant/embedded/bin
-
-    # libxml: xml2-config, xmlcatalog, xmllint
-    rm opt/vagrant/embedded/bin/{xml2-config,xmlcatalog,xmllint}
-    ln -s ${libxml2}/bin/xml2-config opt/vagrant/embedded/bin
-    ln -s ${libxml2}/bin/xmlcatalog opt/vagrant/embedded/bin
-    ln -s ${libxml2}/bin/xmllint opt/vagrant/embedded/bin
-
-    # libxslt: xslt-config, xsltproc
-    rm opt/vagrant/embedded/bin/{xslt-config,xsltproc}
-    ln -s ${libxslt}/bin/xslt-config opt/vagrant/embedded/bin
-    ln -s ${libxslt}/bin/xsltproc opt/vagrant/embedded/bin
 
     mkdir -p "$out"
     cp -r opt "$out"
